59da64f940a747cf589971bab997831fbe6589be
codekarimi <ckarimi676@gmail.com>
2024-09-02 19:06:19 +0000

    Authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+Authentication class to manage API
+"""
+from flask import request
+from typing import TypeVar, List
+
+
+class Auth:
+    """
+    Auth base class for authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Make sure path is not in excluded_paths
+        """
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Check validity of the header
+        """
+        if request is None or 'Authorization' not in request.headers:
+            return None
+
+        return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Get the current user based on a request
+        """
+        return None
